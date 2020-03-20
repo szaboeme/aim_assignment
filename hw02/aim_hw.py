43a8c356ec9497f749446f52fb0d8280ff996a9c
Emese Szabó <szaboeme@fel.cvut.cz>
2020-03-20 18:30:05 +0100

    added hw2 base

--- hw02/aim_hw.py
+++ hw02/aim_hw.py
@@ -2,57 +2,53 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.io as sio
 import os.path
+import os
 import itertools
 
-def nonlincontrast(value, alpha):
-    gamma = 1 / (1 - alpha)
-    if value < 0.5:
-        newvalue = 0.5 * pow(2*value, gamma)
-    else:
-        newvalue = 1 - (0.5 * pow(2 - 2*value, gamma))
-
-    return newvalue
-
 if __name__ ==  "__main__":
-    # read image
-    img = plt.imread( 'Lena.png' )
-    
-    # show original
-    plt.imshow( img )
+    print("Welcome to PPAFFT (Primitive Python Application For Fourier Transform)!")
+    imgs = os.listdir('images/')
+    print("\nPlease choose an image from the list to be loaded:\n")
+    for i in imgs:
+        print(i)
+    img_name = input("\nSelected image name (without '.png'):  ")
+    file_name = "images/" + img_name + ".png"
 
-    neg = img.copy()
-    tresh = img.copy()
-    bri = img.copy()
-    cont = img.copy()
-    gamma = img.copy()
-    nl_cont = img.copy()
+    # check if existing image
+    if not os.path.isfile(file_name):
+        try:
+            sys.exit(0)
+        finally:
+            print("Invalid file name! Exiting.")
 
-    # modify image
-    for i in range(0, 256):
-        for j in range(0, 256):
-            # negatice image
-            neg[i, j] = (1 - img[i, j])
-            # threshold = 0.5
-            if img[i, j, 0] < 0.5:
-                tresh[i, j, :] = 0
-            else:
-                tresh[i, j, :] = 1
-            # brightness +0.2
-            v = img[i, j, 0] + 0.2
-            bri[i, j, :] = max(min(v, 1), 0)
-            # contrast 1.25
-            v = img[i, j, 0] * 1.25
-            cont[i, j, :] = max(min(v, 1), 0)
-            # gamma 2.0
-            gamma[i, j] = pow(img[i, j], 0.5)
-            # non-linear contrast
-            nl_cont[i, j, :] = nonlincontrast(img[i, j, 0], 0.5)
+    # read chosen image
+    img = plt.imread( file_name )
 
+    # if more than 2 image dimensions
+    if (img.ndim > 2):
+        img = img[:,:,0] # take one channel only
 
-    # save new images
-    plt.imsave('negative.png', neg)
-    plt.imsave('treshold.png', tresh)
-    plt.imsave('brightness.png', bri)
-    plt.imsave('contrast.png', cont)
-    plt.imsave('gamma.png', gamma)
-    plt.imsave('nl_contrast.png', nl_cont)
+    fig = plt.figure()
+
+    plt.subplot(131), plt.imshow(img, cmap='gray'), plt.title("Original"), plt.axis('off')
+
+    f = np.fft.fft2(img)
+    fabs = np.abs(f)
+    flog = np.log(fabs)
+    fimg = flog / np.max(flog)
+
+    plt.subplot(132), plt.imshow(fimg, cmap='gray'), plt.title("Spectrum"), plt.axis('off')
+
+    fshift = np.fft.fftshift(f)
+    fabs = np.abs(fshift)
+    flog = np.log(fabs)
+    fimg = flog / np.max(flog)
+
+    plt.subplot(133), plt.imshow(fimg, cmap='gray'), plt.title("Spectrum with shift"), plt.axis('off')
+
+    plt.show()
+
+    # save shifted spectrum image
+    plt.imsave('results/'+img_name+'_spectrum.png', fimg, cmap='gray')
+    # save full figure with original image and spectrum side by side
+    # plt.savefig('results/'+img_name+'_figure.png')
